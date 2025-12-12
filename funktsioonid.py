5d92eccb9098df0ce2fc62f78f215fd3c55dd500
Vins Dominiks Ragels <87246484+ragels4@users.noreply.github.com>
2025-12-12 15:27:56 +0200

    Refactor funktsioonid.py functions and comments
    Fixed function definitions and comments in funktsioonid.py.

--- funktsioonid.py
+++ funktsioonid.py
@@ -1,5 +1,6 @@
 from random import *
 
+#добавил a, b в скобки
 def vahetus(a, b):
     abi=a
     a=b
@@ -7,19 +8,21 @@ def vahetus(a, b):
     return a,b
 
 def generaator(n,loend,a,b):
+    #добавил букву n в скобки и добавил один tab после 11 строчки и правильно написал loend.append
     for i in range(n):
         loend.append(randint(a,b))
     
 def jagamine(loend: list, p: list, n: list, nol: list):
     for el in loend:
         if el>0:
-            p.append(el)
-        elif el<0:
+            p.append(el)  # исправил правильно append
+        elif el<0:        #убрал лишнее двоеточие
             n.append(el)
         else:
             nol.append(el)
 
 def keskmine(loend):
+    #правильно поставил табы
     n=len(loend)
     if n==0:
         kesk=0
@@ -31,5 +34,6 @@ def keskmine(loend):
     return kesk
 
 def lisamine(loend,el):
+    #правильно написал функцию
     loend.append(el)
     loend.sort()
